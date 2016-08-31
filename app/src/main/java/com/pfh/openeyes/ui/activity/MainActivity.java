053a36ab27f730368f8d79b3e4a2239d81d08612
afayp <pan464324435@qq.com>
2016-08-31 23:54:05 +0800

    Initial commit

---
+++ app/src/main/java/com/pfh/openeyes/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pfh.openeyes.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pfh.openeyes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
